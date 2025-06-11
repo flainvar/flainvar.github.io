90bf023d82af0b1ae30561c402a3234df3a151fb
flainvar <fmarquezastacio@gmail.com>
2025-06-11 21:26:55 +0200

    update site info

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -80,7 +80,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Portafolio de ciberseguridad.',
-        text: "Aquí podrás aprender junto a mí sobre análisis de seguridad defensiva.",
+        text: "¡Hola! Bienvenidos a mi portafolios, aquí podrás aprender junto a mí sobre análisis de seguridad defensiva. Tras mi etapa universitaria -estudiando sociología- decidí dedicarme a una de mis pasiones tempranas: la informática. Así, me matriculé en el Grado Superior de ASIR a la par que estudio, de manera autodidáctica, ciberseguridad con herramientas online como HackTheBox. Mi objetivo actual es la certificación de CDSA.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
