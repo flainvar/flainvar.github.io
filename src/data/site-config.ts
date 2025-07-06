5f79e24c82442c806c3fc874851df1731b67b0db
flainvar <fmarquezastacio@gmail.com>
2025-07-06 18:48:23 +0200

    prueba

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -67,10 +67,10 @@ const siteConfig: SiteConfig = {
         },
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Portafolio de ciberseguridad.',
+        text: "¡Hola! Bienvenidos a mi portafolios, aquí podrás aprender junto a mí sobre análisis de seguridad defensiva. Tras mi etapa universitaria -estudiando sociología- decidí dedicarme a una de mis pasiones tempranas: la informática. Así, me matriculé en el Grado Superior de ASIR a la par que estudio, de manera autodidáctica, ciberseguridad con herramientas online como HackTheBox. Mi objetivo actual es la certificación de CDSA.",
         image: {
-            src: '/hero.jpeg',
+            src: '/curro.png',
             alt: 'A person sitting at a desk in front of a computer'
         },
     },
